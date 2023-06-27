b75d1b57677f044f57fdb849bae9c901948a8ee9
Sage Choi <coolcuds@gmail.com>
2023-06-27 11:37:17 -0700

    Establish relationship and add to_dict and from_dict

--- app/models/card.py
+++ app/models/card.py
@@ -7,5 +7,24 @@ class Card(db.Model):
 
     # TODO: Decide if this needs to be added? Would need to do flask db migrate, then upgrade again
     # TODO: Would need to add foreign key in card.py, and relationship to board
-    # board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
-    # board = db.relationship("Board", back_populates="cards")
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    board = db.relationship("Board", back_populates="cards")
+
+    def to_dict(self):
+        card_dict = {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count
+        }
+
+        # Set relationship between board and card
+        if self.board_id:
+            card_dict["board_id"] = self.board_id
+
+        return card_dict
+
+    @classmethod
+    def from_dict(cls, card_data):
+        return cls(
+            message=card_data["message"]
+        )
