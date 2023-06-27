d865712ff508d05abedd28158d00b7bc29554a9f
Sage Choi <coolcuds@gmail.com>
2023-06-27 11:14:17 -0700

    Add comment about relationship between boards and cards

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,8 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+
+    # TODO: Decide if this needs to be added? Would need to do flask db migrate, then upgrade again
+    # TODO: Would need to add foreign key in card.py, and relationship to board
+    # board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    # board = db.relationship("Board", back_populates="cards")
