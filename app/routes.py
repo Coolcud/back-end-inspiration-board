f4702ae375ffaea067cb406830f3fe809518194f
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-07-17 11:57:41 -0700

    changed slack bot message slightly

--- app/routes.py
+++ app/routes.py
@@ -63,7 +63,7 @@ def send_post_to_slack(card):
     load_dotenv()
     slack_bot_token = os.environ.get('SLACK_BOT_TOKEN')
     slack_channel = 'byte-sized-inspiration'
-    text = f"A new card was created with this message:\'{card.message}\'"
+    text = f"A new card was created with this message: \"{card.message}\""
     headers = {'Authorization': f"Bearer {slack_bot_token}"}
     data = {
         'channel': slack_channel,
